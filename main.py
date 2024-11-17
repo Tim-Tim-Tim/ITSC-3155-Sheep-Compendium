189503834f6f1e80bc8b6cfa9995af7a90d8b623
Tim-Tim-Tim <timgs2002@gmail.com>
2024-11-17 17:02:15 -0500

    Added extra comments for further explanation

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ app = FastAPI()
 
 @app.get("/sheep/{id}", response_model=Sheep)
 def read_sheep(id: int):
-    return db.get_sheep(id)
+    # Check if the sheep ID exists
+    if id in db.data:
+        return db.get_sheep(id)
+    # In case sheep ID does not exist
+    else:
+        raise HTTPException(status_code=404, detail="Sheep not found")
+
 
 @app.post("/sheep/", response_model=Sheep, status_code=status.HTTP_201_CREATED)
 def add_sheep(sheep: Sheep):
@@ -18,6 +24,8 @@ def add_sheep(sheep: Sheep):
     # Add the new sheep to the database
     db.data[sheep.id] = sheep
     return sheep # Return the newly added sheep data
+
+
 
 @app.delete("/sheep/{id}", response_model=Sheep, status_code=status.HTTP_200_OK)
 def delete_sheep(id: int):
@@ -32,6 +40,8 @@ def delete_sheep(id: int):
     else:
         raise HTTPException(status_code=404, detail="Sheep not found")
 
+
+
 @app.put("/sheep/{id}", response_model=Sheep, status_code=status.HTTP_200_OK)
 def update_sheep(id: int, sheep: Sheep):
         # Check if the sheep ID exists
@@ -44,6 +54,8 @@ def update_sheep(id: int, sheep: Sheep):
         else:
             raise HTTPException(status_code=404, detail="Sheep not found")
 
+
 @app.get("/sheep/", response_model=list[Sheep])
 def read_sheep():
+    #Return the list of sheep stored in data
     return list(db.data.values())
